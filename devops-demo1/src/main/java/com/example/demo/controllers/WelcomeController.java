c0761f1565b1648aa60a89cb073d30dc8206b70f
manish <user601@host.docker.internal>
2023-10-26 11:43:22 +0530

    Service got created

--- devops-demo1/src/main/java/com/example/demo/controllers/WelcomeController.java
+++ devops-demo1/src/main/java/com/example/demo/controllers/WelcomeController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.services.WelcomeService;
 
 @RestController
 @RequestMapping
 public class WelcomeController {
+	private WelcomeService service;
 	
+	@Autowired
 	public String[] getCity(){
-		return new String[] {"hyd","mumbai","kerala"};
+		return this.service.getCity();
 	}
 
 }
